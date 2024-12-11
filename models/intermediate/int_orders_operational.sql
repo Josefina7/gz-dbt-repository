ea49ac07d2f9c340d64cb4b5d2d2ec61e7246980
Josefina Angulo <josefina.angulooliva@gmail.com>
2024-12-11 16:05:22 +0000

    operational margin per order

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,16 @@
+SELECT
+    margin.products_id,
+    margin.orders_id,
+    margin.date_date,
+    margin.revenue,
+    margin.quantity,
+    margin.purchase_cost,
+    margin.margin,
+    ship.shipping_fee,
+    ship.logcost AS log_cost,
+    ship.ship_cost,
+    ROUND((margin.margin + ship.shipping_fee - ship.logcost - ship.ship_cost),2) AS operational_margin
+FROM {{ref("int_sales_margin")}} as margin 
+JOIN {{ref("stg_data__ship")}} as ship 
+USING (orders_id)
+ORDER BY orders_id
