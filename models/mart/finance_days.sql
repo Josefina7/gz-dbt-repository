1b6c9849c66d3ebcbd9f9491587a6f9dc9194e1f
Josefina Angulo <josefina.angulooliva@gmail.com>
2024-12-11 19:14:31 +0000

    finance_day

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,13 +1,13 @@
-SELECT
+select
     date_date,
-    COUNT(orders_id) as nb_transactions,
-    ROUND(SUM(revenue),2) as total_revenue,
-    ROUND(SAFE_DIVIDE((SUM(revenue)),COUNT(orders_id))) as average_basket,
-    ROUND(SUM(margin),2) as margin,
-    ROUND(SUM(operational_margin),2) as operational_margin,
-    ROUND(SUM(purchase_cost),2) as purchase_cost,
-    ROUND(SUM(shipping_fee),2) as shipping_fee,
-    ROUND(SUM(log_cost),2) as log_cost,
-    COUNT(quantity) as quantity
-FROM {{ref("int_orders_operational")}}
-GROUP BY date_date
+    count(orders_id) as nb_transactions,
+    round(sum(revenue), 2) as total_revenue,
+    round(safe_divide((sum(revenue)), count(orders_id))) as average_basket,
+    round(sum(margin), 2) as margin,
+    round(sum(operational_margin), 2) as operational_margin,
+    round(sum(purchase_cost), 2) as purchase_cost,
+    round(sum(shipping_fee), 2) as shipping_fee,
+    round(sum(log_cost), 2) as log_cost,
+    count(quantity) as quantity
+from {{ ref("int_orders_operational") }}
+group by date_date
