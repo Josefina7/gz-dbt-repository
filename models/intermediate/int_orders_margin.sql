6ee1b39e3e6cb5e93cfdf5d22c7e0aa8742ff289
Josefina Angulo <josefina.angulooliva@gmail.com>
2024-12-11 16:19:16 +0000

    int orders margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,6 +5,6 @@ SELECT
   ROUND(SUM(quantity),2) AS quantity,
   ROUND(SUM(purchase_cost),2) AS purchase_cost,
   ROUND(SUM(margin),2) AS margin
-FROM `circular-fusion-442116-q8.dbt_jangulo.int_sales_margin`
+FROM {{ref("int_sales_margin")}}
 GROUP BY orders_id, date_date
 ORDER BY orders_id
